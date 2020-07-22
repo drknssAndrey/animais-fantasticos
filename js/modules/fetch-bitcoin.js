54816a80821ec2441e2263009299e98686dbafd2
Andrey Balbino <andreymelo98@gmail.com>
2020-07-22 10:32:05 -0300

    primeiro commit

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,15 @@
+export default function initFetchBitcoin() {
+  fetch("https://blockchain.info/ticker").then((response) => {
+    response
+      .json()
+      .then((bitcoin) => {
+        const btcPreco = document.querySelector(".btc-preco");
+        btcPreco.innerHTML = (100 / bitcoin.BRL.sell).toFixed(4);
+      })
+      .catch((erro) => {
+        console.log(Error(erro));
+      });
+  });
+}
+
+// https://blockchain.info/ticker
