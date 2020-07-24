3e384b8809ca628eca35b468f69caf68d872324d
Andrey Balbino <andreymelo98@gmail.com>
2020-07-23 22:52:42 -0300

    menu mobile refatorado

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
-import initMenuMobile from "./modules/menu-mobile.js";
+import MenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
@@ -12,13 +12,16 @@ import ScrollAnima from "./modules/scroll-anima.js";
 
 const scrollSuave = new ScrollSuave("[data-menu='suave'] a[href^='#']");
 scrollSuave.init();
+
 const accordion = new Accordion("[data-anime='accordion'] dt");
 accordion.init();
+
 const tabNav = new TabNav(
   "[data-tab='menu']  li",
   "[data-tab='content'] section"
 );
 tabNav.init();
+
 const modal = new Modal(
   "[data-modal='abrir'",
   "[data-modal='fechar'",
@@ -28,11 +31,16 @@ modal.init();
 
 const tooltip = new Tooltip("[data-tooltip]");
 tooltip.init();
+
 const scrollAnima = new ScrollAnima("[data-anime='scroll']");
 scrollAnima.init();
+
 const dropdownMenu = new DropdownMenu("[data-dropdown]");
 dropdownMenu.init();
-initMenuMobile();
+
+const menuMobile = new MenuMobile("[data-menu='button']", "[data-menu='list']");
+menuMobile.init();
+
 initFuncionamento();
 
 fetchAnimais("../animais-api.json", ".numeros-grid");
